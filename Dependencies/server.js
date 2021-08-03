fa715d6aff95058bf9303da2fb133c6d70884d35
jason@jasonaddison.dev <28279470+Jason-Addison@users.noreply.github.com>
2021-08-02 21:24:59 -0400

    Initial

---
+++ Dependencies/server.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var request = require('request');
+// Add your credentials:
+// Add your client ID and secret
+var CLIENT =
+  'AVMnV5rtEOnhtx1653IIduMhfLHo7uLIaLMJ5y-8lD5O1JdjAAg8q_59o5iNhBWocsYEa3QGKYupt5vn';
+var SECRET =
+  'EIb9hfB6B-JyfKzxKHm0A_sHIAArxoegiiiBPbzmhk57pcCDGNjU8jslyQu9gpe8339-nVRaNLUg_Lie';
+var PAYPAL_API = 'https://api-m.sandbox.paypal.com';
+express()
+  // Set up the payment:
+  // 1. Set up a URL to handle requests from the PayPal button
+  .post('/my-api/create-payment/', function(req, res)
+  {
+    // 2. Call /v1/payments/payment to set up the payment
+    request.post(PAYPAL_API + '/v1/payments/payment',
+    {
+      auth:
+      {
+        user: CLIENT,
+        pass: SECRET
+      },
+      body:
+      {
+        intent: 'sale',
+        payer:
+        {
+          payment_method: 'paypal'
+        },
+        transactions: [
+        {
+          amount:
+          {
+            total: '5.99',
+            currency: 'USD'
+          }
+        }],
+        redirect_urls:
+        {
+          return_url: 'https://minecraft.net',
+          cancel_url: 'https://youtube.com'
+        }
+      },
+      json: true
+    }, function(err, response)
+    {
+      if (err)
+      {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+      // 3. Return the payment ID to the client
+      res.json(
+      {
+        id: response.body.id
+      });
+    });
+  })
+  // Execute the payment:
+  // 1. Set up a URL to handle requests from the PayPal button.
+  .post('/my-api/execute-payment/', function(req, res)
+  {
+    // 2. Get the payment ID and the payer ID from the request body.
+    var paymentID = req.body.paymentID;
+    var payerID = req.body.payerID;
+    // 3. Call /v1/payments/payment/PAY-XXX/execute to finalize the payment.
+    request.post(PAYPAL_API + '/v1/payments/payment/' + paymentID +
+      '/execute',
+      {
+        auth:
+        {
+          user: CLIENT,
+          pass: SECRET
+        },
+        body:
+        {
+          payer_id: payerID,
+          transactions: [
+          {
+            amount:
+            {
+              total: '10.99',
+              currency: 'USD'
+            }
+          }]
+        },
+        json: true
+      },
+      function(err, response)
+      {
+        if (err)
+        {
+          console.error(err);
+          return res.sendStatus(500);
+        }
+        // 4. Return a success response to the client
+        res.json(
+        {
+          status: 'success'
+        });
+      });
+  }).listen(3000, function()
+  {
+    console.log('Server listening at http://localhost:3000/');
+  });
+// Run `node ./server.js` in your terminal
